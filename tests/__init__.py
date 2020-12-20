70a1c3aab7690dc05fd3736629d0031acca9d259
Stefan Holdermans <stefan@holdermans.nl>
2020-12-20 01:34:29 +0100

    Hook up unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2020 Stefan Holdermans.
+# Licensed under the MIT License.
+
+"""Tests for pyray."""
---
+++ tests/test_pyray.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2020 Stefan Holdermans.
+# Licensed under the MIT License.
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+"""Unit tests for pyray."""
+
+import unittest
+
+
+class TestPyray(unittest.TestCase):
+
+    def test_dummy(self):
+        pass
